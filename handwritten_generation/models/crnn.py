bc0aef339304c46740d570784a92106f61af5e8c
sameertantry <sameertantry@gmail.com>
2024-05-06 05:33:19 +0300

    added crnn build function

--- handwritten_generation/models/crnn.py
+++ handwritten_generation/models/crnn.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import timm
 
+from omegaconf import DictConfig
+
 class BidirectionalLSTM(nn.Module):
     def __init__(self, inputs_size: int, hidden_size: int, output_size: int):
         super().__init__()
@@ -12,31 +14,32 @@ class BidirectionalLSTM(nn.Module):
         outputs, _ = self.lstm(x)
 
         return self.linear(outputs)
+    
+def build_encoder_from_config(config: DictConfig) -> nn.Module:
+    timm_model = timm.create_model(config.name, pretrained=False)
+    encoder = nn.Sequential(
+        timm_model.conv_stem,
+        timm_model.bn1,
+        timm_model.blocks[:config.num_layers],
+        nn.Conv2d(in_channels=timm_model.blocks[config.num_layers - 1][0].conv.out_channels, out_channels=config.out_channels, kernel_size=(2, 3), padding=(0, 1), bias=False),
+        nn.BatchNorm2d(config.out_channels),
+        nn.ReLU(True),
+    )
 
+    return encoder
 
 class CRNN(nn.Module):
-    def __init__(self, num_classes: int, num_channels: int = 3):
+    def __init__(self, config: DictConfig):
         super().__init__()
 
-        backbone = timm.create_model('mobilenetv3_small_050.lamb_in1k', pretrained=False)
-        self.convs = nn.Sequential(
-            backbone.conv_stem,
-            backbone.bn1,
-            backbone.blocks[:-1],
-        )
-        self.neck = nn.Sequential(
-            nn.Conv2d(in_channels=backbone.blocks[-2][0].conv.out_channels, out_channels=256, kernel_size=2, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(True),
-        )
+        self.encoder = build_encoder_from_config(config.encoder)
         self.rnn = nn.Sequential(
-            BidirectionalLSTM(512, 256, 256),
-            BidirectionalLSTM(256, 256, num_classes)
+            BidirectionalLSTM(config.encoder.out_channels, config.rnn_hidden_size, config.rnn_hidden_size),
+            BidirectionalLSTM(config.rnn_hidden_size, config.rnn_hidden_size, config.num_classes)
         )
 
     def forward(self, x):
-        x = self.convs(x) # batch_size x 288 x 2 x 8
-        x = self.neck(x)  # bathc_size x 256 x 1 x 8
+        x = self.encoder(x)
         x = x.squeeze(2)
         x = x.transpose(1, 2)
 
